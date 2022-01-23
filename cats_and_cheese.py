9f14c2ca22094e89f29b36056891173c3e6efc79
alvarodiez20 <54044015+alvarodiez20@users.noreply.github.com>
2022-01-23 18:06:15 +0100

    water_jugs() implemented with tests and an example notebook is added

--- cats_and_cheese.py
+++ cats_and_cheese.py
@@ -1,5 +1,7 @@
+from collections import deque
+
 # Challenge 1
-def balanced_symbols(input_string: str=""):
+def balanced_symbols(input_string: str="")->bool:
     """
     Checks whether an input string is properly balanced with respect to '(' and ')', 
     '[' and ']' and '{' and '}'. The function ignores the symbols within comments, a comment starts 
@@ -66,7 +68,7 @@ def balanced_symbols(input_string: str=""):
         return False
 
 # Challenge 2
-def cost_prorating(cost: int, weights : list):
+def cost_prorating(cost: int, weights : list)->list:
     """
     Returns the distribution of a cost proportionally to a set of  weights, maintaining the order 
     of the input weights. The total amount of the distribution is the input cost.
@@ -153,3 +155,159 @@ def cost_prorating(cost: int, weights : list):
         diff = sum(rounded_cost_distribution) - cost
 
     return rounded_cost_distribution
+
+
+# Challenge 3
+def water_jugs(target:float, capacities: list) -> str:
+    """
+    Given a set of jugs' capacities and a target volume, this function returns a minimal sequence
+    of pouring operations to reach a state where at least one jug has the target volume of water in it.
+    If no sequence exists, it returns None.
+
+    BFS algorithm is used for this implementation of N-water jugs.
+
+    Parameters
+    ----------
+    target: float
+        Target volume of water.
+    capacities: list
+        Set of jugs' capacities.
+    
+    Returns
+    -------
+    str
+        The minimum steps in format source -> destination : states.
+        Where:
+            source: the jug to pour from, or -1 to fill from the faucet
+            destination: the jug to pour into, or -1 to empty into the sink
+            states: the state of each jug, i.e. its volume of water, after the pouring operation
+
+    Examples
+    --------
+    >>> water_jugs(target=4, capacities=[3, 5])
+    -1 -> 1 : (0, 5)
+    1 -> 0 : (3, 2)
+    0 -> -1 : (0, 2)
+    1 -> 0 : (2, 0)
+    -1 -> 1 : (2, 5)
+    1 -> 0 : (3, 4)
+    >>> water_jugs(target=15, capacities=[1, 2, 10, 20])
+    -1 -> 3 : (0, 0, 0, 20)
+    3 -> 0 : (1, 0, 0, 19)
+    3 -> 1 : (1, 2, 0, 17)
+    1 -> -1 : (1, 0, 0, 17)
+    3 -> 1 : (1, 2, 0, 15)
+    """
+    if type(target) != int and type(target) != float:
+        raise TypeError("The parameter 'target' should be a float.")
+    
+    if target <= 0:
+        raise ValueError("The 'target' should be greater than zero.")
+    
+    if type(capacities) != list:
+        raise TypeError("The parameter 'capacities' should be a list of integers.")
+    
+    if len(capacities) == 0:
+        raise ValueError("The 'capacities' list should not be empty.")
+    
+    for capacity in capacities:
+        if type(capacity) != int and type(capacity) != float:
+            raise TypeError("Each of the jugs' capacities should be float or int.")
+        if capacity <= 0:
+            raise ValueError("Each of the weights should be greater than 0.")
+        
+    def fill_a_jug(state, capacities):
+        possible_states, prev_states, pouring_ops = [], [], []
+        for i in range(len(capacities)):
+            temp = list(state)
+            temp[i] = capacities[i]
+            possible_states.append(temp)
+            prev_states.append(state)
+            pouring_ops.append([-1, i])
+
+        return possible_states, prev_states, pouring_ops
+
+    def empty_a_jug(state):
+        possible_states, prev_states, pouring_ops = [], [], []
+        for i in range(len(state)):
+            temp = list(state)
+            temp[i] = 0
+            possible_states.append(temp)
+            prev_states.append(state)
+            pouring_ops.append([i, -1])
+        
+        return possible_states, prev_states, pouring_ops
+
+    def pour_a_jug_into_another(state, capacities):
+        possible_states, prev_states, pouring_ops = [], [], []
+        for i in range(len(state)):
+            for e in range(len(state)):
+                temp = list(state)
+                if e == i: continue
+                diff = min(state[i], capacities[e] - temp[e])
+                temp[i] = temp[i] - diff
+                temp[e] = temp[e] + diff
+                possible_states.append(temp)
+                prev_states.append(state)
+                pouring_ops.append([i, e])
+                
+        return possible_states, prev_states, pouring_ops
+    
+    states_queue = deque()
+    ops_queue = deque()
+    prevs_queue = deque()
+    path = []
+    ops = []
+    prevs = []
+    searched = []
+    solution = ""
+
+    states_queue.append([0 for i in range(len(capacities))])
+    ops_queue.append([0 for i in range(len(capacities))])
+    prevs_queue.append([0, 0])
+
+    while len(states_queue) > 0:
+        state = states_queue.popleft()
+        op = ops_queue.popleft()
+        prev = prevs_queue.popleft()
+
+        if state in searched:
+            continue
+        
+        path.append(state)
+        ops.append(op)
+        prevs.append(prev)
+        searched.append(state)
+
+        for s in state:
+            if s == target:
+                i = len(path) - 1
+                while i != 0:
+                    state_tuple = tuple(path[i])
+                    solution = "{} -> {} : {}\n".format(ops[i][0], ops[i][1], state_tuple) + solution
+                    i = path.index(prevs[i])
+                return solution
+
+        # fill a jug from the faucet
+        possible_states, prev_states, pouring_ops = fill_a_jug(state, capacities)
+        for possible_state, prev_state, pouring_op in zip(possible_states, prev_states, pouring_ops):
+            states_queue.append(possible_state)
+            prevs_queue.append(prev_state)
+            ops_queue.append(pouring_op)
+
+        # empty a jug in the sink
+        possible_states, prev_states, pouring_ops = empty_a_jug(state)
+        for possible_state, prev_state, pouring_op in zip(possible_states, prev_states, pouring_ops):
+            states_queue.append(possible_state)
+            prevs_queue.append(prev_state)
+            ops_queue.append(pouring_op)
+
+        # pour a jug into another jug
+        possible_states, prev_states, pouring_ops = pour_a_jug_into_another(state, capacities)
+        for possible_state, prev_state, pouring_op in zip(possible_states, prev_states, pouring_ops):
+            states_queue.append(possible_state)
+            prevs_queue.append(prev_state)
+            ops_queue.append(pouring_op)
+                
+
+
--- test_cats_and_cheese.py
+++ test_cats_and_cheese.py
@@ -1,5 +1,5 @@
 import unittest 
-from cats_and_cheese import balanced_symbols, cost_prorating
+from cats_and_cheese import balanced_symbols, cost_prorating, water_jugs
 
 class TestCatsAndCheese(unittest.TestCase):
     
@@ -29,7 +29,7 @@ class TestCatsAndCheese(unittest.TestCase):
     
     def test_cost_prorating_type_error(self):
         self.assertRaises(TypeError, cost_prorating, 1.0, [1])
-        self.assertRaises(TypeError, cost_prorating, 1, "hola")
+        self.assertRaises(TypeError, cost_prorating, 1, "hi")
         self.assertRaises(TypeError, cost_prorating, 1, [1.0])
     
     def test_cost_prorating_value_error(self):
@@ -38,7 +38,24 @@ class TestCatsAndCheese(unittest.TestCase):
         self.assertRaises(ValueError, cost_prorating, 1, [-1])
         self.assertRaises(ValueError, cost_prorating, 1, [0])
 
-
+    # Challenge 3
+    def test_water_jugs(self):
+        self.assertEqual(water_jugs(4, [3, 5]), '-1 -> 1 : (0, 5)\n1 -> 0 : (3, 2)\n0 -> -1 : (0, 2)\n1 -> 0 : (2, 0)\n-1 -> 1 : (2, 5)\n1 -> 0 : (3, 4)\n')
+        self.assertEqual(water_jugs(15, [1, 2, 10, 20]), '-1 -> 3 : (0, 0, 0, 20)\n3 -> 0 : (1, 0, 0, 19)\n3 -> 1 : (1, 2, 0, 17)\n1 -> -1 : (1, 0, 0, 17)\n3 -> 1 : (1, 2, 0, 15)\n')
     
+    def test_water_jugs_returns_a_str(self):
+        self.assertIsInstance(water_jugs(4, [3, 5]), str)
+
+    def test_water_jugs_type_error(self):
+        self.assertRaises(TypeError, water_jugs, "hi", [1])
+        self.assertRaises(TypeError, water_jugs, 1, 1)
+        self.assertRaises(TypeError, water_jugs, 1, [1.0, "hi"])
+    
+    def test_water_jugs_value_error(self):
+        self.assertRaises(ValueError, water_jugs, -1, [1])
+        self.assertRaises(ValueError, water_jugs, 1, [])
+        self.assertRaises(ValueError, water_jugs, 1, [-1])
+
+
 if __name__ == "__main__":
     unittest.main()
